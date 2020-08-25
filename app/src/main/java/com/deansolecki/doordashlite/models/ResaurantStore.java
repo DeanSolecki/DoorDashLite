24ccc3389821d2759df591aea75be2efd48a2509
Dean Solecki <deansolecki@gmail.com>
2020-08-25 15:30:48 -0700

    Fix compilation error with gradle configuration and fix Typo in model name.

--- app/src/main/java/com/deansolecki/doordashlite/models/ResaurantStore.java
+++
@@ -1,4 +0,0 @@
-package com.deansolecki.doordashlite.models;
-
-public class ResaurantStore {
-}
---
+++ app/src/main/java/com/deansolecki/doordashlite/models/RestaurantStore.java
@@ -0,0 +1,7 @@
+package com.deansolecki.doordashlite.models;
+
+public class RestaurantStore {
+
+    private static RestaurantStore mRestaurantStore;
+
+}
