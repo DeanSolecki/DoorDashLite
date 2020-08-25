7e3a05724f7c70bb1b63db13fdfe2d60fa3377ea
Dean Solecki <deansolecki@gmail.com>
2020-08-25 15:24:28 -0700

    Add feed fragment view and enable databinding in app.gradle.

--- app/src/main/java/com/deansolecki/doordashlite/FeedActivity.java
+++ app/src/main/java/com/deansolecki/doordashlite/FeedActivity.java
@@ -1,10 +1,16 @@
 package com.deansolecki.doordashlite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class FeedActivity extends AppCompatActivity {
+public class FeedActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new FeedFragment();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
