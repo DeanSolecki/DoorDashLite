2d4da8cfbebb673df1616eba8cdc93e8e51367c3
Dean Solecki <deansolecki@gmail.com>
2020-08-26 12:43:08 -0700

    Add scrollview to detailfragment layout so that on landscape user can scroll to see all information.

---
+++ app/src/test/java/com/deansolecki/doordashlite/viewmodels/RestaurantViewModelTest.java
@@ -0,0 +1,37 @@
+package com.deansolecki.doordashlite.viewmodels;
+
+import android.view.View;
+
+import com.deansolecki.doordashlite.models.Restaurant;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class RestaurantViewModelTest {
+    private Restaurant mRestaurant;
+    private View.OnClickListener mOnClickListener;
+    private RestaurantViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        mOnClickListener = mock(View.OnClickListener.class);
+        mRestaurant = mock(Restaurant.class);
+        mSubject = new RestaurantViewModel();
+        mSubject.setRestaurant(mRestaurant);
+        mSubject.setOnClickListener(mOnClickListener);
+    }
+
+    @Test
+    public void getRestaurant() {
+        assertThat(mRestaurant.name, is(mSubject.getRestaurant().name));
+    }
+
+    @Test
+    public void getOnClickListener() {
+        assertThat(mOnClickListener, is(mSubject.getOnClickListener()));
+    }
+}
