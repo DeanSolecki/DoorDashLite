0d8326b7eb6c1720b641a6f7fefe816d038b3297
Dean Solecki <deansolecki@gmail.com>
2020-08-26 13:34:20 -0700

    Add instrumentation tests and clean up file names.

---
+++ app/src/androidTest/java/com/deansolecki/doordashlite/ActivityInstrumentationTest.java
@@ -0,0 +1,45 @@
+package com.deansolecki.doordashlite;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.deansolecki.doordashlite.api.DoorDashService;
+import com.jakewharton.espresso.OkHttp3IdlingResource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.IsAnything.anything;
+
+@RunWith(AndroidJUnit4.class)
+public class ActivityInstrumentationTest {
+    @Test
+    public void loadFeedFragment() {
+        ActivityScenario<FeedActivity> scenario = ActivityScenario.launch(FeedActivity.class);
+        onView(withId(R.id.feed_recycler_view))
+                .check(matches(anything()));
+    }
+
+    @Test
+    public void transitionToDetailFragment() {
+        // Note: requires animations disabled.
+        // ToDo: This happens to be the only test that waits for an api call, but idlingResource could be registered with a helper.
+        IdlingResource idlingResource = OkHttp3IdlingResource.create("OkHttp", DoorDashService.getOkHttpClient());
+        IdlingRegistry.getInstance().register(idlingResource);
+        ActivityScenario<FeedActivity> scenario = ActivityScenario.launch(FeedActivity.class);
+        onView(withId(R.id.feed_recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+        onView(withId(R.id.detail_constraint_layout))
+                .check(matches(anything()));
+    }
+
+}
--- app/src/main/java/com/deansolecki/doordashlite/api/DoorDashService.java
+++ app/src/main/java/com/deansolecki/doordashlite/api/DoorDashService.java
@@ -1,5 +1,6 @@
 package com.deansolecki.doordashlite.api;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,17 +8,24 @@ public class DoorDashService {
     public static final String LAT = "37.422740";
     public static final String LNG = "-122.139956";
     private static DoorDashApi mDoorDashApi;
+    private static OkHttpClient mOkHttpClient;
 
     public static DoorDashApi getApi() {
         if (mDoorDashApi == null) {
+            mOkHttpClient = new OkHttpClient();
             mDoorDashApi = buildDoorDashApi();
         }
         return mDoorDashApi;
     }
 
+    public static OkHttpClient getOkHttpClient() {
+        return mOkHttpClient;
+    }
+
     private static DoorDashApi buildDoorDashApi() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.doordash.com/v2/")
+                .client(mOkHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(DoorDashApi.class);
